35fa9d5003cb9ac39f429e2ffdee6a4ba4f7bb8d
kaviyarasan <a.kavi1947@gmail.com>
2024-10-21 23:04:26 +0530

    Added homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ import React from 'react';
 const Home = () => {
   return (
      <div id="homeContainer"> <h1>Welcome to Kavi Learning Site  </h1></div>
-    
+   
   );
 };
 
 export default Home;
+
